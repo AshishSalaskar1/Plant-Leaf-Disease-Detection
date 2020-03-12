91d9edde3a25c3a96d8a5c16f1fb2b940271228a
AshishSalaskar1 <ashishsalaskar1@gmail.com>
2020-03-12 21:29:11 +0530

    added train_stats

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import matplotlib.pylab as plt
 import numpy as np
 # import cv2
 from skimage.transform import resize
+from flask_cors import CORS, cross_origin
+import model_train
 # from keras.backend import clear_session
 
 categories=['Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_','Corn_(maize)___healthy','Peach___Bacterial_spot','Peach___healthy']
  
 
-
+def train_model():
+    return_metrics = model_train.train_model()
+    return return_metrics
 
 def getPrediction():
     img = plt.imread('uploads/test.jpg')
@@ -21,18 +25,15 @@ def getPrediction():
     sortProb = np.argsort(prob[0,:])
     return categories[sortProb[-1]]
     
-
-# print(getPrediction())
-
-
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
 import os
 import urllib.request
 from flask import Flask, request, redirect, jsonify, render_template
 from werkzeug.utils import secure_filename
 
+
 app = Flask(__name__,static_url_path="/static")
+cors = CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -40,29 +41,38 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     print(request.files)
-#     # checking if the file is present or not.
-#     if 'file' not in request.files:
-#         return "No file found"
-
-#     file = request.files['file']
-#     file.save("static/test.jpg")
-#     return "file successfully saved"
 @app.route('/home')
+@cross_origin()
 def upload_form():
 	return render_template('upload.html')
 
+@app.route('/train')
+@cross_origin()
+def train_form():
+    return_metrics = train_model()
+    return jsonify(return_metrics)
+
+
+
+@app.route('/test')
+@cross_origin()
+def test_fn():
+    data = {"test_accuracy":0.9090909361839294,"test_loss":0.2362778918309645,"train_acc":[0.3465346395969391,0.6707921028137207,0.7301980257034302,0.8589109182357788,0.8861386179924011,0.9084158539772034,0.9504950642585754,0.9678217768669128,0.9603960514068604,0.9678217768669128],"train_loss":[1.5189724712088557,0.9187239137026343,0.582697061618956,0.37744680401122216,0.3049584026678954,0.22594869977766924,0.14196028095660823,0.10174716522197912,0.12360440991302528,0.09216664952806908],"val_acc":[0.5643564462661743,0.5841584205627441,0.7326732873916626,0.8910890817642212,0.8415841460227966,0.8811880946159363,0.9504950642585754,0.9306930899620056,0.9207921028137207,0.9405940771102905],"val_loss":[1.2257271745417377,0.8644408707571501,0.6019150957022563,0.32721499582328417,0.43055959739307365,0.2777669866486351,0.14636361097345257,0.16552021361813687,0.19681064600106513,0.17120785996465399]}
+    return jsonify(data)
+
+
+@app.route('/getAllDiseases')
+def getALlDiseases():
+    return jsonify({"No_of_Disease":len(categories),"Diseases":categories})
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def home():
     return app.send_static_file('index.html')
 
-
-
-
+#Dual get post
 @app.route('/api/file-upload', methods=['GET', 'POST'])
+@cross_origin()
 def upload_file():
 	# check if the post request has the file part
     if 'file' not in request.files:
@@ -92,3 +102,14 @@ def upload_file():
 
 if __name__ == "__main__":
     app.run()
+
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     print(request.files)
+#     # checking if the file is present or not.
+#     if 'file' not in request.files:
+#         return "No file found"
+
+#     file = request.files['file']
+#     file.save("static/test.jpg")
+#     return "file successfully saved"
---
+++ model_train.py
@@ -0,0 +1,200 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+tf.enable_eager_execution()
+
+import tensorflow.keras
+from tensorflow.python.keras.models import Sequential,Input,Model
+from tensorflow.python.keras.layers import Dense, Dropout, Flatten
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.python.keras.layers.normalization import BatchNormalization
+from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
+
+# from skimage import io
+
+from sklearn.model_selection import train_test_split
+
+from skimage.transform import resize
+from skimage import io
+from skimage import img_as_ubyte
+
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+# import cv2
+
+def train_model():
+
+    datadir1="Datasets/Testing_Dataset"
+    datadir="Datasets/Training_Dataset"
+    training_data=[]
+    testing_data=[]
+    categories=['Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_','Corn_(maize)___healthy','Peach___Bacterial_spot','Peach___healthy']
+
+
+
+
+    def create_training_data():
+        for category in categories:
+            path=os.path.join(datadir,category)
+            class_num=categories.index(category)
+            for img in os.listdir(path):
+                # img_array=cv2.imread(os.path.join(path,img))
+                # img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+                # new_array=cv2.resize(img_array,(28,28))
+
+                img_array = io.imread(os.path.join(path,img))
+                img_array = img_array[:, :, ::-1]
+                new_array = resize(img_array,(28,28))
+                new_array = img_as_ubyte(new_array)
+              
+                training_data.append([new_array,class_num])
+
+    def create_testing_data():
+        for category in categories:
+            path=os.path.join(datadir1,category)
+            class_num=categories.index(category)
+            for img in os.listdir(path):
+                #opencv code
+                # img_array=cv2.imread(os.path.join(path,img))
+                # img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+                # new_array=cv2.resize(img_array,(28,28))
+
+                img_array = io.imread(os.path.join(path,img))
+                img_array = img_array[:, :, ::-1]
+                new_array = resize(img_array,(28,28))
+                new_array = img_as_ubyte(new_array)
+                
+                testing_data.append([new_array,class_num])
+
+
+
+    create_training_data()
+    create_testing_data()
+
+
+
+
+    train_X=[]
+    train_Y=[]
+
+    for feature,label in training_data:
+        train_X.append(feature)
+        train_Y.append(label)
+    type(train_X)
+
+    test_X=[]
+    test_Y=[]
+    for feature,label in testing_data:
+        test_X.append(feature)
+        test_Y.append(label)
+
+
+
+    train_X=np.array(train_X)#.reshape(-1,28,28,1)
+    train_Y=np.array(train_Y)
+    test_X=np.array(test_X)#.reshape(-1,28,28,1)
+    test_Y=np.array(test_Y)
+    #type(train_X)
+
+    # get_ipython().run_line_magic('matplotlib', 'inline')
+
+    # print('Training data shape : ', train_X.shape, train_Y.shape)
+
+    # print('Testing data shape : ', test_X.shape, test_Y.shape)
+
+
+
+
+    classes = np.unique(train_Y)
+    nClasses = len(classes)
+    # print('Total number of outputs : ', nClasses)
+    # print('Output classes : ', classes)
+
+
+
+    train_X = train_X.reshape(-1, 28,28, 3)
+    test_X = test_X.reshape(-1, 28,28, 3)
+    train_X.shape, test_X.shape
+
+    train_X = train_X.astype('float32')
+    test_X = test_X.astype('float32')
+    train_X = train_X / 255.
+    test_X = test_X / 255.
+
+
+
+
+    # Change the labels from categorical to one-hot encoding
+    train_Y_one_hot = to_categorical(train_Y)
+    test_Y_one_hot = to_categorical(test_Y)
+
+
+
+
+
+    train_X,valid_X,train_label,valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
+    train_X.shape,valid_X.shape,train_label.shape,valid_label.shape
+
+
+    batch_size = 32
+    epochs = 10
+    num_classes = 5
+
+
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,3),padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D((2, 2),padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+    model.add(LeakyReLU(alpha=0.1))                  
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Flatten())
+    model.add(Dense(128, activation='linear'))
+    model.add(LeakyReLU(alpha=0.1))                  
+    model.add(Dense(num_classes, activation='softmax'))
+
+
+
+    model.compile(loss=tensorflow.keras.losses.categorical_crossentropy, optimizer=tensorflow.keras.optimizers.Adam(),metrics=['accuracy'])
+    # model.summary()
+
+    model_train = model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+
+
+    test_eval = model.evaluate(test_X, test_Y_one_hot, verbose=0)
+    # print('Test loss:', test_eval[0])
+    # print('Test accuracy:', test_eval[1])
+    test_loss = float(test_eval[0])
+    test_accuracy = float(test_eval[1])
+
+
+
+    train_loss = model_train.history['loss']
+    val_loss = model_train.history['val_loss']
+
+    train_acc = model_train.history['acc']
+    val_acc = model_train.history['val_acc']
+
+
+
+
+    return_value = dict()
+    return_value['train_loss'] = [float(i) for i in train_loss] 
+    return_value['val_loss'] = [float(i) for i in val_loss]
+    return_value['train_acc'] = [float(i) for i in train_acc]
+    return_value['val_acc'] = [float(i) for i in val_acc]
+    return_value['test_loss'] = test_loss
+    return_value['test_accuracy'] = test_accuracy
+
+    print(return_value)
+
+
+    return return_value
+
+
+# train_model()
