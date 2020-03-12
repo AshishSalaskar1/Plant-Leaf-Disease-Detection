963f97821eda973e34550748cc5e9458a6c0243d
AshishSalaskar1 <ashishsalaskar1@gmail.com>
2020-03-12 22:20:27 +0530

    CORS no_params

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__,static_url_path="/static")
-cors = CORS(app)
+CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -42,12 +42,12 @@ def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/home')
-@cross_origin()
+
 def upload_form():
 	return render_template('upload.html')
 
 @app.route('/train')
-@cross_origin()
+
 def train_form():
     return_metrics = train_model()
     return jsonify(return_metrics)
@@ -55,24 +55,25 @@ def train_form():
 
 
 @app.route('/test')
-@cross_origin()
+
 def test_fn():
     data = {"test_accuracy":0.9090909361839294,"test_loss":0.2362778918309645,"train_acc":[0.3465346395969391,0.6707921028137207,0.7301980257034302,0.8589109182357788,0.8861386179924011,0.9084158539772034,0.9504950642585754,0.9678217768669128,0.9603960514068604,0.9678217768669128],"train_loss":[1.5189724712088557,0.9187239137026343,0.582697061618956,0.37744680401122216,0.3049584026678954,0.22594869977766924,0.14196028095660823,0.10174716522197912,0.12360440991302528,0.09216664952806908],"val_acc":[0.5643564462661743,0.5841584205627441,0.7326732873916626,0.8910890817642212,0.8415841460227966,0.8811880946159363,0.9504950642585754,0.9306930899620056,0.9207921028137207,0.9405940771102905],"val_loss":[1.2257271745417377,0.8644408707571501,0.6019150957022563,0.32721499582328417,0.43055959739307365,0.2777669866486351,0.14636361097345257,0.16552021361813687,0.19681064600106513,0.17120785996465399]}
     return jsonify(data)
 
 
 @app.route('/getAllDiseases')
+
 def getALlDiseases():
     return jsonify({"No_of_Disease":len(categories),"Diseases":categories})
 
 @app.route('/', methods=['GET'])
-@cross_origin()
+
 def home():
     return app.send_static_file('index.html')
 
 #Dual get post
 @app.route('/api/file-upload', methods=['GET', 'POST'])
-@cross_origin()
+
 def upload_file():
 	# check if the post request has the file part
     if 'file' not in request.files:
